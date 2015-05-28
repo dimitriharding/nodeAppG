3b286cf21bf6286ef9ef6d5d7d28c76e00cca853
Gavin Samuels <gsamuels@qualityworkscg.com>
2015-05-28 03:46:20 -0500

    all files

---
+++ app.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Database------------------------------------------------->
+var config = {       "USER"    : "",                  
+           "PASS"    : "",       
+           "HOST"    : "ec2-52-11-254-37.us-west-2.compute.amazonaws.com",         
+           "PORT"    : "27017",        
+           "DATABASE" : "nodeDB"     };
+
+var dbPath  = "mongodb://"+config.USER + ":"+     config.PASS + "@"+     config.HOST + ":"+    config.PORT + "/"+     config.DATABASE; 
+
+
+           
+var mongo = require('mongoskin');
+var db = mongo.db(dbPath, {native_parser:true});
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router--------------------->
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
+console.log('starting Express (NodeJS) Web server'); 
+app.listen(8080); 
+console.log('Webserverlistening on port 8080');
